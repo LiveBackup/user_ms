22fd239eb1bb5a06812dcef0a816a43ab6734db9
Juan Diego <jdiegopm12@gmail.com>
2023-03-08 18:54:11 -0500

    Unit tests added

--- src/__tests__/unit/services/account.service.spec.ts
+++ src/__tests__/unit/services/account.service.spec.ts
@@ -3,7 +3,7 @@ import {
 } from '@loopback/testlab';
 import {AccountRepository} from '../../../repositories';
 import {AccountService} from '../../../services';
-import {givenAccountCredentials, givenRepositories} from '../../helpers/database.helpers';
+import {givenAccount, givenAccountCredentials, givenRepositories} from '../../helpers/database.helpers';
 import {givenServices} from '../../helpers/services.helpers';
 
 describe('Unit testing - AccountCredentials Model', () => {
@@ -47,5 +47,37 @@ describe('Unit testing - AccountCredentials Model', () => {
   });
 
   describe('Database methods', () => {
+    it('Creates a new Account', async () => {
+      const account = givenAccount();
+      await accountService.create(account);
+
+      const savedAccount = await accountRepository.findOne({where: {email: account.email}});
+      expect(savedAccount).not.to.be.null();
+      expect(savedAccount?.email).to.be.equal(account.email);
+      expect(savedAccount?.username).to.be.equal(account.username);
+    });
+
+    it('Finds an account by its email', async () => {
+      const account = givenAccount();
+      await accountService.create(account);
+
+      const savedAccount = await accountService.findByEmail(account.email);
+      expect(savedAccount).not.to.be.null();
+      expect(savedAccount?.email).to.be.equal(account.email);
+      expect(savedAccount?.username).to.be.equal(account.username);
+    });
+
+    it('Finds an account by its username', async () => {
+      const account = givenAccount();
+      await accountService.create(account);
+
+      const savedAccount = await accountService.findByUsername(account.email);
+      expect(savedAccount).not.to.be.null();
+      expect(savedAccount?.email).to.be.equal(account.email);
+      expect(savedAccount?.username).to.be.equal(account.username);
+    });
+
+    it('Creates the Account Credentials', async () => {
+    });
   });
 });
